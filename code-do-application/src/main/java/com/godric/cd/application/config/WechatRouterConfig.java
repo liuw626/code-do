885f0275beeba7ef1910690ccf9c0fdf43814d96
Godric <436066640@qq.com>
2022-02-15 14:02:56 +0800

    router config

---
+++ code-do-application/src/main/java/com/godric/cd/application/config/WechatRouterConfig.java
@@ -0,0 +1,60 @@
+package com.godric.cd.application.config;
+
+import com.godric.cd.handler.CodeDoWechatHandler;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WechatRouterConfig {
+
+    @Autowired
+    private WxMpService wxMpService;
+
+//    @Autowired
+//    private LoginHandler loginHandler;
+
+    @Autowired
+    private CodeDoWechatHandler messageHandler;
+
+//    @Autowired
+//    private SubscribeHandler subscribeHandler;
+
+    @Bean
+    public WxMpMessageRouter getWxMsgRouter() {
+        WxMpMessageRouter router = new WxMpMessageRouter(wxMpService);
+//        // 登录
+//        router.rule()
+//                .async(false)
+//                .msgType(XmlMsgType.TEXT)
+//                .rContent("登录|登陆")
+//                .handler(loginHandler)
+//                .end();
+        // 收到消息
+        router.rule()
+                .async(false)
+                .msgType(WxConsts.XmlMsgType.TEXT)
+                .handler(messageHandler)
+                .end();
+//        // 关注
+//        router.rule()
+//                .async(false)
+//                .msgType(XmlMsgType.EVENT)
+//                .event(EventType.SUBSCRIBE)
+//                .handler(subscribeHandler)
+//                .end();
+//        // 点击按钮
+//        router.rule()
+//                .async(false)
+//                .msgType(XmlMsgType.EVENT)
+//                .event(EventType.CLICK)
+//                .eventKey(CommonConstant.LOGIN_MENU_KEY)
+//                .handler(loginHandler)
+//                .end();
+        return router;
+    }
+
+}
