12f6605c27934e3a8b29806042ee4c9cda13ebf2
Godric <436066640@qq.com>
2022-02-15 17:50:25 +0800

    BizErrorEnum

--- code-do-common/src/main/java/com/godric/cd/exception/BizErrorEnum.java
+++ code-do-common/src/main/java/com/godric/cd/exception/BizErrorEnum.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BizErrorEnum {
 
+    SYSTEM_ERROR("SYSTEM_ERROR", "系统开小差了, 请稍后再试!"),
     NEED_LOGIN("NEED_LOGIN", "请先登录!"),
+    PARAM_CANNOT_NULL("PARAM_CANNOT_NULL", "参数 %s 不能为空"),
     OPERATION_FAILED("OPERATION_FAILED", "操作失败"),
     NOT_MP_MESSAGE("NOT_MP_MESSAGE", "不是来自公众号的消息"),
     INVALID_MP_REQUEST("INVALID_MP_REQUEST", "非法的公众号请求"),
